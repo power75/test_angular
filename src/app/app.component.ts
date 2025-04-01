aa7cb710e78bc0a749ae7a015b423b110aaf6072
power75 <karurangod@outlook.jp>
2025-04-01 22:22:22 +0900

    item list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [CommonModule],
 })
 export class AppComponent {
-  title = 'todo applcation';
+  componentTitle = "My To Do List";
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  addItem(description: string) {
+    if (!description) return;
+
+    this.allItems.unshift({//unshiftで配列の先頭に要素を追加。引数descriptinでdoneは常にfalse
+      description,
+      done: false
+    });
+  }
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems; //filterがallなら全てのItem配列を返す。
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done 
+    ); //this.filterがdoneならdone: trueの配列を返す。
+  }    //それ以外ならdone: falseの配列を返す。
 }
